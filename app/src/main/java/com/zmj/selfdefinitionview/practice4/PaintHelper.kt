2ab0b49ce2a6cb2ffd928bafac924f2f5138865b
ZmjDns <292570927@qq.com>
2019-12-10 13:43:43 +0800

    canvas.clip使用

--- app/src/main/java/com/zmj/selfdefinitionview/practice4/PaintHelper.kt
+++ app/src/main/java/com/zmj/selfdefinitionview/practice4/PaintHelper.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
-import android.view.ContextMenu
 import android.view.View
 import com.zmj.selfdefinitionview.R
 
@@ -34,6 +34,40 @@ class PaintClipView:View{
         //二维平移，即上下左右平移
         //canvas?.translate(80f,0f)
         canvas?.drawBitmap(bitmap,0f,0f,paint)
+        canvas?.restore()
+    }
+}
+
+class CanvasClipPath: View{
+    constructor(context:Context):super(context)
+    constructor(context: Context,attr:AttributeSet?):super(context,attr)
+    constructor(context: Context,attr: AttributeSet?,defStyle:Int):super(context,attr,defStyle)
+
+    val paint = Paint()
+    val path = Path()
+    val path2 = Path()
+    val bitmap = BitmapFactory.decodeResource(resources,R.drawable.map_icon)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 2f
+
+        path.addCircle(100f,100f,80f,Path.Direction.CCW)
+        canvas?.drawPath(path,paint)
+
+        canvas?.save()
+        canvas?.clipPath(path)
+        canvas?.drawBitmap(bitmap,10f,10f,paint)
+        canvas?.restore()
+
+        path2.addCircle(300f,100f,40f,Path.Direction.CW)
+
+        canvas?.drawPath(path2,paint)
+
+        canvas?.save()
+        canvas?.clipPath(path2)
+        canvas?.drawBitmap(bitmap,200f,0f,paint)
         canvas?.restore()
     }
 }
