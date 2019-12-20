6a35eb025ffea40c2abf2c719275c4411c535ae4
ZmjDns <292570927@qq.com>
2019-12-20 20:23:54 +0800

    动画介绍

---
+++ app/src/main/java/com/zmj/selfdefinitionview/practice6/AnimateInfo.kt
@@ -0,0 +1,23 @@
+package com.zmj.selfdefinitionview.practice6
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/12/20
+ * Description :
+ *
+ * 1.动画分为两类：
+ *      Animation  和   Transition
+ *
+ * 2.Animation也分两类：
+ *      View Animation   和   Property Animation
+ *      View Animation是存粹基于frameWork的绘制转变
+ *
+ *
+ *
+ *
+ *
+ */
+class AnimateInfo {
+}
