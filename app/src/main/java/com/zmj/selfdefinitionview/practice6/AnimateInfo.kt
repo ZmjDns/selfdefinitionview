f38f3372d1ef66368155d0d246947b8968ef4343
ZmjDns <292570927@qq.com>
2019-12-21 21:26:19 +0800

    动画介绍

--- app/src/main/java/com/zmj/selfdefinitionview/practice6/AnimateInfo.kt
+++ app/src/main/java/com/zmj/selfdefinitionview/practice6/AnimateInfo.kt
@@ -11,12 +11,12 @@ package com.zmj.selfdefinitionview.practice6
  *      Animation  和   Transition
  *
  * 2.Animation也分两类：
- *      View Animation   和   Property Animation
- *      View Animation是存粹基于frameWork的绘制转变
- *
- *
- *
- *
+ *      ViewAnimation   和   Property Animation
+ *      ViewAnimation是存粹基于frameWork的绘制转变
+ *      Property Animation 属性动画  现在项目中99%的动画都在用它
+ *      不仅可以通过自带的属性也可以自定义实现
+ *  3.Transition 在Android中是指切换界面时的动画效果，
+ *      逻辑上实现比较复杂，重点在于切换而不是动画
  *
  */
 class AnimateInfo {
