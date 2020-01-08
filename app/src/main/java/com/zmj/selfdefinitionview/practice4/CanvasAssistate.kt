60ee15675cb83f90194e2d3a43d7e41baafec282
ZmjDns <292570927@qq.com>
2020-01-08 22:42:31 +0800

    重新进行canvas辅助学习

---
+++ app/src/main/java/com/zmj/selfdefinitionview/practice4/CanvasAssistate.kt
@@ -0,0 +1,11 @@
+package com.zmj.selfdefinitionview.practice4
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2020/1/8
+ * Description : 重新进行canvas辅助学习
+ */
+class CanvasAssistant {
+}
