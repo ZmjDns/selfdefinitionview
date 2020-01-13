111d69096e739452652dbccf8aec199b24269da9
ZmjDns <292570927@qq.com>
2020-01-13 17:21:25 +0800

    Matrix对图形的几何变换

--- app/src/main/java/com/zmj/selfdefinitionview/practice4/MatrixShape.kt
+++ app/src/main/java/com/zmj/selfdefinitionview/practice4/MatrixShape.kt
@@ -27,7 +27,7 @@ class MatrixShape: View {
     private val bitmap = BitmapFactory.decodeResource(resources, R.drawable.map_icon)
     private val bitmapW = bitmap.width.toFloat()
     private val bitmapH = bitmap.height.toFloat()
-    private val myMatrix = matrix
+    private val myMatrix = Matrix()
 
 
     override fun onDraw(canvas: Canvas?) {
@@ -37,13 +37,20 @@ class MatrixShape: View {
         canvas.drawBitmap(bitmap,0f,0f,paint)
         canvas.restore()
 
+        canvas.save()
         myMatrix.reset()
-        myMatrix.preTranslate(bitmapW,0f)
+        //Matrix的preXXX()是按顺序执行的
+        //因此执行的顺序为先平移后旋转
+        /*myMatrix.preTranslate(bitmapW * 1.5f,0f)
+        myMatrix.preRotate(45f)*/
+
+        //Matrix的postXXX方法是倒序执行的
+        //因此执行的顺序为，先平移后旋转
+        myMatrix.postRotate(45f)
+        myMatrix.postTranslate(bitmapW * 1.5f,0f)
+
         canvas.concat(myMatrix)
         canvas.drawBitmap(bitmap,0f,0f,paint)
         canvas.restore()
-
-
     }
-
 }
