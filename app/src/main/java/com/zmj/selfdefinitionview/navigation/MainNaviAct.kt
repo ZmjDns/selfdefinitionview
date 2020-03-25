461c2b47d43db345f36c04d6b081c6b8c01f0b4b
ZmjDns <292570927@qq.com>
2020-03-25 21:42:26 +0800

    底部导航view的实现

---
+++ app/src/main/java/com/zmj/selfdefinitionview/navigation/MainNaviAct.kt
@@ -0,0 +1,22 @@
+package com.zmj.selfdefinitionview.navigation
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import com.zmj.selfdefinitionview.R
+import com.zmj.selfdefinitionview.base.BaseActivity
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/3/24
+ * Description :
+ */
+class MainNaviAct: BaseActivity(){
+    override val layoutRid: Int
+        get() = R.layout.act_navigation
+
+    override fun initFirst(savedInstanceState: Bundle?){}
+
+}
